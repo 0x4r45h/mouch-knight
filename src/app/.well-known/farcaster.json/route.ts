6bc10ccab6a14537fa60fcac473a118433660205
0x4r45h <0x4r45h@gmail.com>
2025-05-21 21:35:05 +0330

    fix manifest file

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from "next/server";
+import {NextResponse} from "next/server";
 import {APP_URL} from "@/config";
 
 export async function GET() {
     const farcasterConfig = {
-        // accountAssociation: {
-        //     "header": "",
-        //     "payload": "",
-        //     "signature": ""
-        // },
+        accountAssociation: {
+            header: "eyJmaWQiOjEwNzgwNzYsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhFM0YzOTY0MjNjRTAyNzUyN2I0ODI1YTIyNzE3ZTVjMDFkY2Y5RDNjIn0",
+            payload: "eyJkb21haW4iOiJtb3VjaC1rbmlnaHQtZmFyY2FzdGVyLmVtYmVyc3Rha2UueHl6In0",
+            signature: "MHgyZDAxNzExMjhiYTU4OTYwYTRlNWI2YTg3MzQwYzM0MzRkMzc1M2Y1ZDg4Y2Y1ZmIyMTU3Y2JhOTRjNThjNGJkMDI5OGM2Mzk4NzUxMzI4NjBjMGY0MDM2NDc0ODlmOTg5YmVkZGY3MmY1MmMxNjc4NTU3ZjY3ODYxNWZkZjJkNDFi"
+        },
         frame: {
             version: "1",
             name: "Mouch Knight",
@@ -20,7 +20,7 @@ export async function GET() {
                 `${APP_URL}/images/ss2.png`,
                 `${APP_URL}/images/ss3.png`,
             ],
-            tags: ["monad", "farcaster", "miniapp", "minigame", "onchain","game"],
+            tags: ["monad", "farcaster", "miniapp", "minigame", "onchain", "game"],
             primaryCategory: "games",
             buttonTitle: "Play Mouch Knight",
             splashImageUrl: `${APP_URL}/images/splash.png`,
